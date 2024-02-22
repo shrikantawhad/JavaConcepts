bb99175fce2f9d3be858debc894265d279899c5f
Shrikant <shrikant.awhad.58@gmail.com>
2024-02-22 11:21:46 +0530

    changes updated

--- src/main/java/String/SubString.java
+++ src/main/java/String/SubString.java
@@ -2,7 +2,8 @@ package String;
 
 public class SubString {
     public static void main(String[] args) {
-        String str = "Hello World";
+        String str = "Hel" +
+                "lo World";
         System.out.println("\n"+str.substring(3,str.length()));
         System.out.println(str.substring(4));
     }
